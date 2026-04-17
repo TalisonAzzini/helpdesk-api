7da638e243f6ea4ce5a95c5ba1a4d4a91ad79b40
Talison Azzini <talison.azzini@gmail.com>
2026-04-17 16:35:42 -0300

    Criação do Services com métodos CRUD para usuários

---
+++ src/main/java/com/helpdesk/helpdesk_api/service/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.helpdesk.helpdesk_api.service;
+
+import com.helpdesk.helpdesk_api.model.Usuario;
+import com.helpdesk.helpdesk_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario dadosUsuario) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+
+        usuario.setNome(dadosUsuario.getNome());
+        usuario.setEmail(dadosUsuario.getEmail());
+        usuario.setCargo(dadosUsuario.getCargo());
+
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public void deletarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
