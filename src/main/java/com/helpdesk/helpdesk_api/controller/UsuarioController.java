eedd010dd70e1251f4eca26713cd68aea7dcaf55
Talison Azzini <talison.azzini@gmail.com>
2026-04-17 17:19:58 -0300

    Criação do Controller do usuário e seus metodos com endpoints.

---
+++ src/main/java/com/helpdesk/helpdesk_api/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.helpdesk.helpdesk_api.controller;
+
+import com.helpdesk.helpdesk_api.model.Usuario;
+import com.helpdesk.helpdesk_api.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(201).body(usuarioService.cadastrarUsuario(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorId(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario dadosAtualizados) {
+        return ResponseEntity.ok(usuarioService.atualizarUsuario(id, dadosAtualizados));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
