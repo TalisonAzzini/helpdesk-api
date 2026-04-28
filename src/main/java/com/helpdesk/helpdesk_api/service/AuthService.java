4729f5647874dfe7794f78c9a4ea4538841a261d
Talison Azzini <talison.azzini@gmail.com>
2026-04-27 21:44:06 -0300

    Estrutura padrão do AuthController e AuthService

---
+++ src/main/java/com/helpdesk/helpdesk_api/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.helpdesk.helpdesk_api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+}
