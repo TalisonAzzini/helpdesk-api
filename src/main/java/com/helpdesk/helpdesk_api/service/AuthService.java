0a30bafb5383d31eed2da7c0e0366c1f9fc79232
Talison Azzini <talison.azzini@gmail.com>
2026-04-30 23:06:12 -0300

    Adicionado Exceptions específicas para cada caso

--- src/main/java/com/helpdesk/helpdesk_api/service/AuthService.java
+++ src/main/java/com/helpdesk/helpdesk_api/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.helpdesk.helpdesk_api.service;
 
+import com.helpdesk.helpdesk_api.infra.exception.UsuarioDuplicadoException;
 import com.helpdesk.helpdesk_api.infra.security.JwtService;
 import com.helpdesk.helpdesk_api.model.Usuario;
 import com.helpdesk.helpdesk_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +15,24 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final JwtService jwtService;
     private final UsuarioRepository usuarioRepository;
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder encoder;
 
     public String autenticarLogin(String email, String senha) {
         Usuario usuario = usuarioRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
 
         if (!encoder.matches(senha, usuario.getSenha())) {
-            throw new RuntimeException("Senha inválida!");
+            throw new BadCredentialsException("Senha inválida!");
         }
 
         return jwtService.gerarToken(usuario);
     }
 
     public Usuario cadastrarUsuario(Usuario usuario) {
+        if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
+            throw new UsuarioDuplicadoException();
+        }
+
         usuario.setSenha(encoder.encode(usuario.getSenha()));
 
         return usuarioRepository.save(usuario);
