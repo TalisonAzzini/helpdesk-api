83d6dd3625104ba564e7259ab3c325fcd447d32b
Talison Azzini <talison.azzini@gmail.com>
2026-05-12 18:22:21 -0300

    :sparkles: feat: adiciona o DTO ChamadoResponse para resposta.

---
+++ src/main/java/com/helpdesk/helpdesk_api/dtos/ChamadoResponse.java
@@ -0,0 +1,16 @@
+package com.helpdesk.helpdesk_api.dtos;
+
+import com.helpdesk.helpdesk_api.enums.Prioridade;
+import com.helpdesk.helpdesk_api.enums.Status;
+import java.time.LocalDateTime;
+
+public record ChamadoResponse(
+        Long id,
+        String titulo,
+        Prioridade prioridade,
+        Status status,
+        LocalDateTime dataCriado,
+        LocalDateTime dataFechado,
+        UsuarioResponse solicitante,
+        UsuarioResponse tecnico
+) {}
