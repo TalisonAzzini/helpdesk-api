094bfc064e4dc4bd74f28fb9661b89a91be300ab
Talison Azzini <talison.azzini@gmail.com>
2026-05-20 23:27:22 -0300

    :recycle: refactor: removido o campo id do DTO ChamadoRequest

--- src/main/java/com/helpdesk/helpdesk_api/dtos/ChamadoRequest.java
+++ src/main/java/com/helpdesk/helpdesk_api/dtos/ChamadoRequest.java
@@ -6,9 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ChamadoRequest(
-        @NotNull
-        Long id,
-
         @NotBlank(message = "Título obrigatório")
         String titulo,
 
