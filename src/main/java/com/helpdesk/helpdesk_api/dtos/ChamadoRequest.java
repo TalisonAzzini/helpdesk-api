64ba8b792a372847a2de74b9e44116cbcac84f88
Talison Azzini <talison.azzini@gmail.com>
2026-05-20 23:24:30 -0300

    :sparkles: feat: Criado um novo DTO ChamadoRequest

---
+++ src/main/java/com/helpdesk/helpdesk_api/dtos/ChamadoRequest.java
@@ -0,0 +1,26 @@
+package com.helpdesk.helpdesk_api.dtos;
+
+
+import com.helpdesk.helpdesk_api.enums.Prioridade;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ChamadoRequest(
+        @NotNull
+        Long id,
+
+        @NotBlank(message = "Título obrigatório")
+        String titulo,
+
+        @NotBlank(message = "Descrição obrigatória")
+        String descricao,
+
+        @NotNull(message = "Prioridade obrigatória")
+        Prioridade prioridade,
+
+        @NotNull(message = "Solicitante obrigatório")
+        Long solicitanteId,
+
+        @NotNull(message = "Tecnico obrigatório")
+        Long tecnicoId
+) {}
